2d06a662e74160df110d25807d674db831b00a24
SatthikGhosh <satthikghosh270@gmail.com>
2024-08-26 23:25:39 +0100

    first commit

---
+++ R/daily_temp.R
@@ -0,0 +1,299 @@
+#' Download and plot essential climate data
+#'
+#' Retrieves the daily air or sea-surface temperature data since 1979 from ClimateReanalyzer.org
+#' Source is University of Maine Climate Change Institute.
+#' \url{https://climatereanalyzer.org/clim/t2_daily/}
+#'
+#' @name get_dailytemp
+#' @param use_cache (boolean) Return cached data if available, defaults to TRUE. Use FALSE to fetch updated data.
+#' @param write_cache (boolean) Write data to cache, defaults to FALSE. Use TRUE to write data to cache for later use. Can also be set using options(hs_write_cache=TRUE)
+#' @param region (string) Region selection, defaults to world air temperature. Options are: World Air "W", Northern Hemisphere Air "NW", Southern Hemisphere Air "SW", Tropics Air "TR", Arctic Air "AR", Antarctica Air "AN", World Sea Surface "WS", and North Atlantic Sea Surface "NS".
+#' @param mean_start (numeric) Start year for historic mean, defaults to 1979.
+#' @param mean_end (numeric) End year for historic mean, defaults to 2000.
+#'
+#' @return Invisibly returns a tibble with the daily 2-meter air or sea surface temperatures since 1940 as well as historic mean by day-of-year and current anomaly versus mean.
+#'
+#' `get_dailytemp` invisibly returns a tibble with the daily temperatures since 1940 as well as mean by day-of-year and anomaly. Default to world data, but region can be selected among six options.
+#'
+#' Region options include world air (default), Northern Hemisphere air, Southern Hemisphere air, Tropics air, Arctic air, Antarctic air, World sea surface and North Atlantic sea surface and is stored in attribute of output.
+#' The historic daily mean-by-day period defaults to 1979-2000. This range can be optionally modified.
+#'
+#' Data are updated daily. For day-of-year mean removes observations from February 29 on leap years.
+#'
+#' @importFrom jsonlite fromJSON
+#' @importFrom utils download.file head tail
+#' @importFrom readr parse_number
+#' @import tidyr
+#' @import dplyr
+#'
+#' @examples
+#' \donttest{
+#' # Fetch temp anomaly from cache if available:
+#' dailytemps <- get_dailytemp()
+#' #
+#' # Force cache refresh:
+#' dailytemps <- get_dailytemp(use_cache=FALSE)
+#' #
+#' # Review cache contents and last update dates:
+#' hockeystick_cache_details()
+#' #
+#' # Plot output using package's built-in ggplot2 settings
+#' plot_dailytemp(dailytemps)
+#'
+#' # Change region to Arctic
+#' arctictemp <- get_dailytemp(region='AR', use_cache=FALSE)}
+#'
+#' @author Hernando Cortina, \email{hch@@alum.mit.edu}
+#' @references
+#' \itemize{
+#' \item ClimateReanalyzer.org: \url{https://climatereanalyzer.org/clim/t2_daily/}
+#'
+#' Notes: daily mean surface air temperature (2-meter height) estimates from the ECMWF Reanalysis version 5 (ERA5) for the period January 1940 to present. ERA5 is a state-of-the-art numerical climate/weather modeling framework that ingests surface, radiosonde, and satellite observations to estimate the state of the atmosphere through time.
+#' ERA5 files have a horizontal grid resolution of 0.25° x 0.25° (about 31km x 31km at 45°N). Each daily temperature represents an average across all model gridcells within the defined latitude/longitude bounds for the selected domain. The means are area-weighted to account for the convergence of longitude lines at the poles
+#'
+#' }
+#'
+#' @export
+
+get_dailytemp <- function(use_cache = TRUE, write_cache = getOption("hs_write_cache"),
+                          region = 'W',
+                          mean_start = if(region %in% c('WS','NS', 'ws', 'ns')) 1982 else 1979,
+                          mean_end = 2000) {
+
+  hs_path <- tools::R_user_dir("hockeystick", "cache")
+  region <- toupper(region)
+
+  if (use_cache) {
+    if (file.exists(file.path(hs_path, 'dailytemp.rds')))
+
+    {cached_temp <- readRDS((file.path(hs_path, 'dailytemp.rds')))
+     cached_region <- attr(cached_temp, "hs_daily_region")
+
+      if (region==cached_region) return(invisible(cached_temp)) }
+  }
+
+  file_url <- switch(region,
+                      W = 'https://climatereanalyzer.org/clim/t2_daily/json/era5_world_t2_day.json',       # World Air
+                     NW = 'https://climatereanalyzer.org/clim/t2_daily/json/era5_nh_t2_day.json',          # Northern Hemi Air
+                     SW = 'https://climatereanalyzer.org/clim/t2_daily/json/era5_sh_t2_day.json',          # Southern Hemi Air
+                     AR = 'https://climatereanalyzer.org/clim/t2_daily/json/era5_arctic_t2_day.json',      # Arctic Air
+                     AN = 'https://climatereanalyzer.org/clim/t2_daily/json/era5_antarctic_t2_day.json',   # #Antarctic Air
+                     TR = 'https://climatereanalyzer.org/clim/t2_daily/json/era5_tropics_t2_day.json',     # Tropics Air
+                     WS = 'https://climatereanalyzer.org/clim/sst_daily/json/oisst2.1_world2_sst_day.json',     # World Sea
+                     NS = 'https://climatereanalyzer.org/clim/sst_daily/json/oisst2.1_natlan1_sst_day.json')    # North Atlantic Sea
+
+                     connected <- .isConnected(file_url)
+  if (!connected) {message("Retrieving remote data requires internet connectivity.\nAvailable regions are: W, NW, SW, AR, AN, TR, WS, NS."); return(invisible(NULL))}
+
+  dl <- tempfile()
+  download.file(file_url, dl)
+  temp_json <- tryCatch (jsonlite::fromJSON(dl),
+                          error=function(error_msg) {
+                            message('Invalid JSON file, please check temperature data url.')
+                            return(NULL)
+                            } )
+  if (is.null(temp_json)) return(invisible(NULL))
+
+leap_years <- seq.int(1940, 2032, 4)
+
+if (region!='WS' && region!='NS') {
+
+suppressWarnings(
+daily_temperature <- as_tibble(temp_json) |> head(-3) |>
+  separate_wider_delim('data', delim=', ', names_sep = '') |>
+  mutate(data1 = parse_number(data1),
+         data366 = parse_number(data366, na = 'NA)')) |>
+  select(-data_source) |> mutate_all(as.numeric) |>
+  pivot_longer(2:367, names_to = 'day_of_year') |>
+  rename(year = name,
+         temp = value) |>
+  mutate(day_of_year = parse_number(day_of_year)) |>
+  filter(!(day_of_year==60 & year %in% leap_years)) |>
+  drop_na()
+) }      else {
+
+  suppressWarnings(
+  daily_temperature <- as_tibble(temp_json) |> head(-3) |> tail(-1) |>
+      separate_wider_delim('data', delim=',', names_sep = '') |>
+    mutate(data1 = parse_number(data1),
+             data366 = parse_number(data366, na = 'NA)')) |>
+    mutate_all(as.numeric) |>
+      pivot_longer(2:367, names_to = 'day_of_year') |>
+      rename(year = name,
+             temp = value) |>
+      mutate(day_of_year = parse_number(day_of_year)) |>
+      filter(!(day_of_year==60 & year %in% leap_years)) |>
+      drop_na()
+  ) }
+
+
+
+daily_temperature <- daily_temperature |> group_by(year) |>
+  mutate(day_of_year = row_number()) |> ungroup() |>
+  group_by(day_of_year) |>
+  mutate(mean_temp = mean(temp[year >= mean_start & year <= mean_end]),
+         temp_anom = temp - mean_temp) |>
+  ungroup()
+
+dates <- data.frame(day_of_year=1:365,
+                    dummy_date=seq(as.Date('1925-01-01'), as.Date('1925-12-31'), by = '1 days'))
+
+daily_temperature <- left_join(daily_temperature, dates, by='day_of_year')
+
+colnames(daily_temperature)[4] <- paste0(mean_start, '-', mean_end, ' mean')
+
+daily_temperature <- mutate(daily_temperature, date = as.Date(paste0(year, '-', substr(dummy_date,6,7), '-', substr(dummy_date, 9, 10))))
+
+daily_temperature <- daily_temperature |> relocate(date, .after=day_of_year)
+
+if (region == 'WS' | region == 'NS') daily_temperature <- daily_temperature |> filter(year!=1981)
+
+attr(daily_temperature, "hs_daily_region") <- region
+
+if (write_cache) saveRDS(daily_temperature, file.path(hs_path, 'dailytemp.rds'))
+
+invisible(daily_temperature)
+}
+
+
+#' Download and plot essential climate data
+#'
+#' Plots the daily temperatures since 1940 and current anomaly data retrieved using `get_dailytemp()` with ggplot2. The output ggplot2 object may be further modified.
+#'
+#'
+#' @name plot_dailytemp
+#' @param dataset Name of the tibble generated by `get_dailytemp`
+#' @param print (boolean) Display daily temperature ggplot2 chart, defaults to TRUE. Use FALSE to not display chart.
+#' @param anomaly (boolean) Display current anomaly versus historic mean, defaults to TRUE.
+#' @param maxtemp (boolean) Display current deviation versus historic max, defaults to FALSE.
+#' @param current_year (numeric) Year to highlight in alternate color, defaults to current year.
+#' @param title_lab (string) Title to override default chart title. Default title pulls region name from dataset attributes.
+#'
+#' @return Invisibly returns a ggplot2 object with daily temperature anomaly chart
+#'
+#' @details `plot_temp` invisibly returns a ggplot2 object with a pre-defined daily temperature anomaly chart using data from `get_dailytemp`.
+#' By default the chart is also displayed. Plots one line per year, as well as mean and anomaly (which may be disabled). Users may further modify the output ggplot2 chart.
+#'
+#' @import ggplot2
+#' @importFrom RColorBrewer brewer.pal
+#'
+#' @examples
+#' \donttest{
+#' # Fetch temperature anomaly:
+#' dailydata <- get_dailytemp()
+#' #
+#' # Plot output using package's built-in ggplot2 defaults
+#' plot_dailytemp(dailydata)
+#'
+#' # Don't plot anomaly shading and highight specific year
+#' plot_dailytemp(anomaly = FALSE, current_year = 2012)
+#'
+#' # Or just call plot_temp(), which defaults to get_temp() dataset
+#' plot_dailytemp()
+#'
+#' p <- plot_dailytemp(dailydata, print = FALSE)
+#' # Modify plot such as: p + ggplot2::labs(title='Record Temperatures in 2023') }
+#'
+#' @author Hernando Cortina, \email{hch@@alum.mit.edu}
+#'
+#' @export
+
+plot_dailytemp <- function(dataset = get_dailytemp(), print = TRUE, anomaly = FALSE, maxtemp = FALSE,
+                           current_year = as.numeric(substr(Sys.Date(), 1, 4)),
+                           title_lab = 'Daily Average Air Temperature') {
+
+  if (is.null(dataset)) return(invisible(NULL))
+
+  if (anomaly && maxtemp) stop("Only one of anomaly or maxtemp parameters can be TRUE")
+
+latest <- pull(tail(dataset, 1)['date'])
+meanperiod <- colnames(dataset)[5]
+colnames(dataset)[5] <- 'mean_temp'
+
+region <- attr(dataset, "hs_daily_region")
+
+subtitle_lab <- '2-meter temperature since 1940 and mean'
+
+if (region == 'WS' ||region == 'NS') subtitle_lab <- 'Sea surface temperature since 1982 and mean'
+
+if (title_lab == "Daily Average Air Temperature") {
+
+    if (region == 'WS' | region == 'NS') title_lab <- 'Daily Average Sea Surface Temperature'
+
+    region <- switch(region,
+                      W = 'World',
+                     NW = 'Northern Hemisphere',
+                     SW = 'Southern Hemisphere',
+                     AR = 'Arctic',
+                     AN = 'Antarctic',
+                     TR = 'Tropics',
+                     WS = 'World (60S-60N)',
+                     NS = 'North Atlantic')
+
+    title_lab <- paste(region, title_lab)}
+
+# Color code notes: 'A': all years, 'M': mean, 'L': latest.
+
+plot <- ggplot(dataset) +
+    geom_line(aes(x = dummy_date, y = temp, group = year, color = 'A'), alpha = 0.7) +
+    scale_fill_gradientn(name = 'Anomaly (C\U00B0)', colors = RColorBrewer::brewer.pal(9, 'YlOrRd'), labels = scales::label_number(accuracy = 0.1)) +
+    geom_line(aes(dummy_date, mean_temp, color = 'M'), linetype = 'dashed', linewidth = 1.1) +
+    scale_y_continuous(n.breaks = 9) +
+    theme_bw(base_size = 12) +
+    scale_x_date(name = 'Day of year', breaks = c(as.Date('1925-01-01'), as.Date('1925-04-01'),
+                                                  as.Date('1925-07-01'), as.Date('1925-10-01'), as.Date('1925-12-31')),
+                 date_labels = '%b%est', date_minor_breaks = '1 month') +
+    labs(title = title_lab, subtitle = subtitle_lab,
+         y = 'Temperature (C\U00B0)',
+         caption = paste0('Source: Climate Change Institute, University of Maine\nClimateReanalyzer.org as of ', latest),
+         color = NULL) +
+    scale_color_manual(values = c('firebrick', 'black', 'grey'), labels = c(current_year, meanperiod, 'All years'), breaks = c('L', 'M', 'A')) +
+    geom_line(data = filter(dataset, year == current_year),
+              aes(dummy_date, temp, color = 'L'), linewidth = 1.3) +
+    theme(legend.position = "top") + theme(legend.key.size = unit(0.5, 'cm'),
+                                         legend.margin = margin(5, 0, 0, 0))
+
+if (anomaly) {
+  subtitle_lab <- '2-meter temperature since 1940, mean, and current anomaly'
+
+  if (region == 'World (60S-60N)' | region == 'North Atlantic' | region == 'WS' | region == 'NS')
+      subtitle_lab <- 'Sea surface temperature since 1982, mean, and current anomaly'
+
+  plot <- plot + scale_fill_gradientn(name = 'Anomaly (C\U00B0)', colors = RColorBrewer::brewer.pal(9, 'YlOrRd'),
+                                      labels = scales::label_number(accuracy = 0.1)) +
+          geom_rect(data = filter(dataset, year == current_year),
+          aes(xmin = dummy_date - 1,
+              xmax = dummy_date,
+              ymin = mean_temp,
+              ymax = mean_temp + temp_anom,
+              fill = temp_anom)) +
+          geom_line(data = filter(dataset, year == current_year),
+            aes(dummy_date, temp, color='L'), linewidth = 1.3) +
+    geom_line(aes(dummy_date, mean_temp, color = 'M'), linetype = 'dashed', linewidth = 1.2) +
+  labs(subtitle = subtitle_lab) }
+
+if (maxtemp) {
+  subtitle_lab <- '2-meter temperature since 1979, mean, and deviation from historic max'
+
+  if (region == 'World (60S-60N)' | region == 'North Atlantic' | region == 'WS' | region == 'AS')
+    subtitle_lab <- 'Sea surface temperature since 1982, mean, and current anomaly'
+
+  max_temp <- dataset |> filter(year!=current_year) |> group_by(day_of_year) |> summarize(max_temp=max(temp))
+  dataset <- left_join(dataset, max_temp, by = "day_of_year")
+
+  plot <- plot +
+    scale_fill_gradientn(name = 'Deviation (C\U00B0)',
+                         colors = rev(RColorBrewer::brewer.pal(9, 'RdYlGn')), labels = scales::label_number(accuracy = 0.1)) +
+    geom_rect(data = filter(dataset, year == current_year),
+              aes(xmin = dummy_date - 1,
+                  xmax = dummy_date,
+                  ymin = max_temp,
+                  ymax = temp,
+                  fill = temp - max_temp)) +
+    geom_line(data = filter(dataset, year == current_year),
+              aes(dummy_date, temp, color='L'), linewidth = 1.3) +
+    labs(subtitle = subtitle_lab) }
+
+  if (print) suppressMessages( print(plot) )
+  invisible(plot)
+}
---
+++ README.md
@@ -0,0 +1,289 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# hockeystick <img src='man/figures/logo.png' align="right" height="139" />
+
+<!-- badges: start -->
+
+[![Lifecycle:
+stable](https://img.shields.io/badge/lifecycle-stable-brightgreen.svg)](https://lifecycle.r-lib.org/articles/stages.html#stable)
+![R-CMD-check](https://github.com/cortinah/hockeystick/workflows/R-CMD-check/badge.svg)
+[![CRAN
+status](https://www.r-pkg.org/badges/version/hockeystick)](https://CRAN.R-project.org/package=hockeystick)
+[![](https://cranlogs.r-pkg.org/badges/hockeystick)](https://cran.r-project.org/package=hockeystick)
+
+<!-- badges: end -->
+
+The goal of `hockeystick` is to make essential Global Heating datasets
+easily available in R to non-climate experts. `hockeystick` users can
+download the latest raw data from authoritative sources as well as view
+it via pre-defined ggplot2 charts. Datasets include atmospheric
+CO<sub>2</sub> and CH<sub>4</sub>, carbon emissions, instrumental,
+reconstructed, and paleo ice-core temperature records, sea levels,
+hurricanes, and Arctic/Antarctic sea-ice.
+
+The choice of data was originally based on [Professor Stefan
+Rahmstorf’s](http://www.pik-potsdam.de/~stefan/) presentation on [The 5
+Most Important Data Sets of Climate
+Science](http://www.pik-potsdam.de/~stefan/5datasets_rahmstorf.pdf). I
+came across this on a post on the [Open Mind
+blog](https://tamino.wordpress.com/2018/11/01/the-5-most-important-data-sets-of-climate-science/).
+I wrote my own
+[post](https://rethinking.rbind.io/2018/11/16/the-top-five-climate-charts-using-ggplot2/)
+on obtaining and visualizing this data (now out of date), which is the
+basis for this package. Additional datasets and visualizations have been
+added over The name of the package stems from the well known
+[hockeystick](https://en.wikipedia.org/wiki/Hockey_stick_graph)
+temperature chart.
+
+`hockeystick` was highlighted on the RStudio
+[RViews](https://rviews.rstudio.com/2021/03/19/february-2021-top-40-new-cran-packages/)
+blog by Joseph Rickert as one of the “Top 40” new packages on CRAN in
+February 2021.
+
+**New in version 0.8.0:** Daily temperature data from
+ClimateReanalyzer.com (see below).
+
+New in version 0.7.0: Globally averaged methane (CH<sub>4</sub>)
+concentration from NOAA.
+
+New in version 0.7.0: Cumulative emissions by country visualization.
+
+New in version 0.6.0: Global CO<sub>2</sub> emissions by region and
+country from GCP.
+
+## Installation
+
+To install the latest `hockeystick` release from CRAN type:
+
+``` r
+install.packages("hockeystick")
+```
+
+You may alternatively install the development version from
+<https://github.com/cortinah/hockeystick> with:
+
+``` r
+remotes::install_github("cortinah/hockeystick")
+```
+
+## Downloading and viewing global heating data
+
+New: Plot daily global temperatures since 1940 and current anomaly:
+
+``` r
+library(hockeystick)
+globaldaily <- get_dailytemp()
+plot_dailytemp(globaldaily)
+```
+
+<img src="man/figures/README-dailytemp-1.png" width="60%" />
+
+Retrieve NOAA/ESRL Mauna Loa CO<sub>2</sub> Observatory concentration
+data and plot:
+
+``` r
+ml_co2 <- get_carbon()
+plot_carbon(ml_co2)
+```
+
+<img src="man/figures/README-carbon-1.png" width="60%" />
+
+Retrieve GCP global CO<sub>2</sub> emissions and plot:
+
+``` r
+emissions <- get_emissions()
+plot_emissions(emissions)
+```
+
+<img src="man/figures/README-emissions-1.png" width="60%" />
+
+``` r
+plot_emissions_with_land(emissions)
+```
+
+<img src="man/figures/README-emissions-2.png" width="60%" />
+
+Visualize cumulative emissions by country:
+
+``` r
+emissions_map()
+```
+
+<img src="man/figures/README-emissionsmap-1.png" width="60%" />
+
+Retrieve NASA/GISS global surface temperature anomaly data and plot:
+
+``` r
+anomaly <- get_temp()
+plot_temp(anomaly)
+```
+
+<img src="man/figures/README-temp-1.png" width="60%" />
+
+Visualize warming using Ed Hawkins styled “warming stripes”:
+
+``` r
+warming_stripes()
+```
+
+<img src="man/figures/README-stripes-1.png" width="60%" />
+
+``` r
+warming_stripes(stripe_only = TRUE, col_strip = viridisLite::viridis(11))
+```
+
+<img src="man/figures/README-stripes2-1.png" width="53%" />
+
+Retrieve tide gauge and satellite sea level data and plot:
+
+``` r
+gmsl <- get_sealevel()
+plot_sealevel(gmsl)
+```
+
+<img src="man/figures/README-sl-1.png" width="60%" />
+
+Retrieve July annual Arctic Sea Ice Index and plot:
+
+``` r
+seaice <- get_seaice()
+plot_seaice(seaice)
+```
+
+<img src="man/figures/README-si-1.png" width="60%" />
+
+`get_seaice()` arguments can be modified to download Antarctic sea ice,
+and allow any month.
+
+You can also visualize sea ice by month and year:
+
+``` r
+arcticice <- get_icecurves()
+plot_icecurves(arcticice)
+```
+
+<img src="man/figures/README-icecurves-1.png" width="60%" />
+
+Retrieve Common Era temperature reconstruction and plot it with
+instrumental record:
+
+``` r
+anomaly2k <- get_temp2k()
+plot_temp2k(anomaly2k)
+```
+
+<img src="man/figures/README-2ktemp-1.png" width="60%" />
+
+Retrieve NOAA HURDAT2 hurricane data and plot:
+
+``` r
+hurricanes <- get_hurricanes()
+plot_hurricanes(hurricanes)
+```
+
+<img src="man/figures/README-hurricanes-1.png" width="60%" />
+
+``` r
+plot_hurricane_nrg(hurricanes)
+```
+
+<img src="man/figures/README-hurricanes-2.png" width="60%" />
+
+Retrieve NOAA/ESRL CH<sub>4</sub> Globally averaged mean data and plot:
+
+``` r
+ch4 <- get_methane()
+plot_methane(ch4)
+```
+
+<img src="man/figures/README-methane-1.png" width="60%" />
+
+Retrieve Vostok paleo ice core data and plot:
+
+``` r
+vostok <- get_paleo()
+plot_paleo(vostok)
+```
+
+<img src="man/figures/README-paleo-1.png" width="60%" />
+
+### Managing the cache
+
+By default, no climate data is cached, and all data is downloaded every
+time any of the `get_` functions is called. To cache data for future
+use, use the `write_cache = TRUE` option, available in all of the `get_`
+functions. To download and cache all data use
+`hockeystick_update_all()`. To view the files, date, and size of cached
+data use `hockeystick_cache_details()`. To re-download data from the
+source use the `use_cache = FALSE` argument in any of the `get_`
+functions, for example:
+`get_carbon(use_cache = FALSE, write_cache = TRUE)`. To delete all
+cached data use `hockeystick_cache_delete_all()`.
+
+Users may also cache data by default by adding
+`options(hs_write_cache = TRUE)`to their script or `.Rprofile` file.
+
+### All together now: climate data grid
+
+``` r
+climate_grid()
+```
+
+<img src="man/figures/README-grid-1.png" width="80%" />
+
+## Acknowledgments
+
+- Carbon Dioxide concentrations: Dr. Pieter Tans, NOAA/GML
+  (<https://gml.noaa.gov/ccgg/trends/>) and Dr. Ralph Keeling, [Scripps
+  Institution of Oceanography](https://scrippsco2.ucsd.edu/).
+- Global temperature anomaly: *GISS Surface Temperature Analysis
+  (GISTEMP), version 4.* GISTEMP Team, 2020: NASA Goddard Institute for
+  Space Studies. <https://data.giss.nasa.gov/gistemp/>
+- Warming Stripes design from Ed Hawkins’ Climate Lab.
+  <https://www.climate-lab-book.ac.uk/2018/warming-stripes/>. In
+  addition grateful to Dr. Dominic Royé for posting his approach to
+  plotting them using ggplot2, which `warming_stripes()` is based on.
+  <https://dominicroye.github.io/en/2018/how-to-create-warming-stripes-in-r/>
+- Sea level data: [NOAA Laboratory for Satellite Altimetry
+  (sat)](https://www.star.nesdis.noaa.gov/socd/lsa/SeaLevelRise/) and
+  [Commonwealth Scientific and Industrial Research Organisation (tide
+  gauges)](https://research.csiro.au/slrwavescoast/sea-level/measurements-and-data/sea-level-data/)
+- Sea Ice Index: [National Snow & Ice Data
+  Center](https://nsidc.org/home). Data Archive:
+  <https://nsidc.org/data/explore-data>
+- Vostok carbon dioxide and temperature data:
+  <https://data.ess-dive.lbl.gov/datasets/doi:10.3334/CDIAC/ATG.009>
+- Common Era reconstructed temperature data: [PAGES2k Consortium and
+  NOAA](https://www.ncei.noaa.gov/access/paleo-search/study/26872)).
+- Hurricanes: National Oceanic and Atmospheric Administration HURDAT
+  Atlantic Hurricane Database [Re-analysis
+  Project](https://www.aoml.noaa.gov/hrd/hurdat/Data_Storm.html),
+  particularly Dr. Chris Landsea.
+- Carbon Dioxide emissions: [Global Carbon
+  Project](https://www.globalcarbonproject.org/carbonbudget/) and [Our
+  World In Data](https://github.com/owid/co2-data)
+- Methane: Lan, X., K.W. Thoning, and E.J. Dlugokencky, NOAA Global
+  Monitoring Laboratory (<https://gml.noaa.gov/ccgg/trends_ch4/>).
+- Thank you to Dirk Eddelbuettel for providing the .isConnected function
+  from his [tint](https://CRAN.R-project.org/package=tint) package to
+  test for internet connectivity.
+- Thank you to Chris Campbell and Steven Bernard of the FT Climate
+  Graphics Explained newsletter for sharing their code for
+  climatereanalyzer.org daily temperature chart (Temperature Records
+  Broken…Again, 9 July 2023). This code is the foundation of the
+  get_dailytemp() and plot_dailytemp() functions.
+
+## Notes and resources
+
+- All data is compiled to the best of my ability from reliable and
+  peer-reviewed sources. Please open an issue if you are aware of
+  enhanced or additional data that may be added to the package. Building
+  `hockeystick` is driven by my interest in tracking climate data and
+  making it easily available to the community.
+- Here are some online resources I have found very helpful to learn mo
+  re about climate science:
+- MIT edX Global Warming Science.
+  <https://www.edx.org/learn/global-warming/massachusetts-institute-of-technology-global-warming-science>
+- SDG Academy: Climate Change: The Science and Global Impact.
+  <https://sdgacademy.org/course/climate-change-the-science-and-global-impact/>
